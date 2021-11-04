797168ecc43f7a65296302e3776722f6204e9e0a
git <pmaasikasp@gmail.com>
2021-11-04 18:08:46 +0200

    add new projects

---
+++ Functions/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // AddTwoNumbers(20, 6);
+            //DivideTwoNumbers(20, 6);
+
+            //kalkulaator:
+            Console.WriteLine("Sisesta esimene arv:");
+            int userNumberOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Sisesta teine arv:");
+            int userNumberTwo = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("vali tehe (sisesta '+', '-', '*' või '/')");
+            char userOperator = Convert.ToChar(Console.ReadLine());
+
+            switch (userOperator)
+            {
+                case '+':
+                    AddTwoNumbers(userNumberOne, userNumberTwo);
+                    break;
+                case '/':
+                    DivideTwoNumbers(userNumberOne, userNumberTwo);
+                    break;
+                case '*':
+                    MultiplyTwoNumbers(userNumberOne, userNumberTwo);
+                    break;
+                case '-':
+                    SubstractTwoNumbers(userNumberOne, userNumberTwo);
+                    break;
+
+                default:
+                    Console.WriteLine("Kena päeva!");
+                    break;
+            }
+
+        }
+
+        public static void AddTwoNumbers(int x, int y) //public, siis võib mujal kasutada, kasutame private siis siin ainult
+        {
+            int result = x + y;
+            Console.WriteLine(result);
+        }
+
+        public static void DivideTwoNumbers(double x, double y)
+        {
+            double result = x / y; //teist tüüpi kümnendarv: decimal (see on aeglasem, aga täpsem kui double)
+            Console.WriteLine(result);
+        }
+
+        //sooritab lahutamist subtraction
+
+        //korrutamine multiplication
+
+        public static void MultiplyTwoNumbers(double x, double y)
+        {
+            double result = x * y;
+            Console.WriteLine(result);
+        }
+
+        public static void SubstractTwoNumbers(int x, int y)
+        {
+            int result = x - y;
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ UserNameDataTask/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace UserNameDataTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajal sisestada eesnime
+            //programm küsib kasutajal sisestada numbrit 1-3
+            //kntrollin, mis ta valis
+            // 3 funktsiooni:
+            //kui kasutaja sisestab '1', siis kuvatakse kasutaja eesnime tagurpidi 
+            //kui kasutaja valib '2', siis kuvatakse eesnime esimest tähte
+            //kui valib '3', siis kuvatakse kasutaja eesnime pikkust
+
+            Console.WriteLine("Sisesta oma eesnimi:");
+            string userName = Console.ReadLine();
+            
+            Console.WriteLine("Sisesta number 1-3.");
+            char userNumber = Convert.ToChar(Console.ReadLine());
+
+
+            switch (userNumber)
+            {
+                case '1':
+                    UserNameReversed(userName);
+                    break;
+
+                case '2':
+                    UserNameChar(userName);
+                    break;
+
+                case '3':
+                    UserNameLength(userName);
+                    break;
+            }
+        }
+
+
+        public static void UserNameReversed(string userInput)
+        {
+            Console.Write("Sinu nimi tagurpidi: ");
+            for (int i = userInput.Length - 1; i >= 0; i--)
+            {
+
+                Console.Write(userInput[i]);
+            }
+        }
+            public static void UserNameChar(string userInput)
+            {
+                Console.WriteLine($"Sinu nime esimene täht on: {userInput[0]}");
+            }
+
+            public static void UserNameLength(string userInput)
+            {
+                Console.WriteLine($"Sinu nime pikkus on: {userInput.Length} sümbolit.");
+            }
+
+        
+
+    }
+}      
